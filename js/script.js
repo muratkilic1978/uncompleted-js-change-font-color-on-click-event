e699c7f7f102808a9cd153f585412068a6ff1b88
Murat Kilic <muratkilic1978@gmail.com>
2021-10-13 07:07:42 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+	/* Variables */
+    /*  Step 1 - Use document.getElementById to fecth the id named "color" and store the value in a variable (let) named "colorElem" followed by a semicolon */
+
+    /* Syntax addEventListener: element.addEventListener(event, function) */
+    /*  Step 2 - add a addEventListener to the variable "colorElem".  The addEventListener should work on a click event and then call the function changeColor followed by a semicolon */
+
+
+
+    /* Step 3 - Create a new variable named "currentColor" with the word "let" and store the value "red" into it followed by a semicolon */
+
+
+    /* Functions */
+    /* Step 4 - Create a function named changeColor() { } with open and end curly-brackets*/
+
+
+    /* Step 4.1 - Inside the curly-brackets in the function - you should create and if else statement */
+
+
+    /* Step 4.2 - if currentColor is equal (==) to "red" then write open and end curly brackets. Inside the open and end curly brackets do following. Use document.querySelector to select 'h1' element in HTML and set the font-color - style.color = "maroon" and finally set the variable currentColor = "maroon" followed by a semicolon  */
+
+
+    /* Step 4.3 - Write else and after the else keyword write begin and end curly-brackets */
+
+    
+    /* Step 4.4 - Inside the curly-brackets use document.querySelector to select 'h1' element and set style.color = "blue" and finally set the currentColor = "red" followed by a semicolon */
+
+
+    /* Ste 4.5 - Place the cursor outside the end-curly brackets from the else-statement and write "return currentColor" followed by a semicolon */
